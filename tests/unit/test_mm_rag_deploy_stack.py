077b4bc2db51f22bb7a94d1d472c252cf54157f0
marcoslashpro <tambascomarco35@gmail.com>
2025-06-11 18:51:44 +0200

    Initial commit

---
+++ tests/unit/test_mm_rag_deploy_stack.py
@@ -0,0 +1,15 @@
+import aws_cdk as core
+import aws_cdk.assertions as assertions
+
+from mm_rag_deploy.mm_rag_deploy_stack import MmRagDeployStack
+
+# example tests. To run these tests, uncomment this file along with the example
+# resource in mm_rag_deploy/mm_rag_deploy_stack.py
+def test_sqs_queue_created():
+    app = core.App()
+    stack = MmRagDeployStack(app, "mm-rag-deploy")
+    template = assertions.Template.from_stack(stack)
+
+#     template.has_resource_properties("AWS::SQS::Queue", {
+#         "VisibilityTimeout": 300
+#     })
